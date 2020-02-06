8f75c2b3178d9c681619da73ed533262265a9d12
Michael Mustapha <mustaphamichael75@gmail.com>
2020-02-06 14:02:28 +0100

    ensures relationship with hub

--- src/models/slot.js
+++ src/models/slot.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose'),
 const SlotSchema = new Schema({
     tag: String,
     status: { type: Number, enum: [0, 1, 2], default: 0 },
-    hub: { type: Schema.Types.ObjectId, ref: 'Hub' }
+    hub: { type: Schema.Types.ObjectId, ref: 'Hub', required: [true, "A slot must have a hub"] }
 })
 
 module.exports = mongoose.model('Slot', SlotSchema)
