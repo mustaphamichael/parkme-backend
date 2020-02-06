6566396b075645f237237b81b3253c010987adb0
Michael Mustapha <mustaphamichael75@gmail.com>
2020-02-06 14:02:58 +0100

    ensures uniqueness of hub

--- src/models/hub.js
+++ src/models/hub.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose'),
-    Schema = mongoose.Schema
+    Schema = mongoose.Schema,
+    uniqueValidator = require('mongoose-unique-validator')
 
 const HubSchema = new Schema({
-    tag: String,
+    tag: { type: String, unique: true, index: true, required: [true, 'Provide a tag'] },
     slots: [{ type: Schema.Types.ObjectId, ref: 'Slot' }]
 })
+HubSchema.plugin(uniqueValidator, { message: 'This hub already exists' });
 
 module.exports = mongoose.model('Hub', HubSchema)
